45f4e339c90615cd23d0647236d613ed680bf878
Julien L <julienlenaz96@gmail.com>
2023-10-22 17:50:21 -0400

    bug fixes

--- src/script.js
+++ src/script.js
@@ -1,17 +1,20 @@
+// Initialize the audio and UI when the window loads
+window.onload = initAudio;
+
+// Global variables
 let audioContext = null;
 let sequence = [];
 let oscillator = null;
 let gainNode = null;
 let filter = null;
 let stepSequencer = [];
-
 let stepIndex = 0;
 let stepInterval = null;
 let tempo = 120;
 let swing = 0.01;
+let constantFrequency = true;
 
-let playingSteps = [];
-
+// Function to start the audio context
 function startAudioContext() {
     audioContext = new (window.AudioContext || window.webkitAudioContext)();
     if (audioContext.state === 'suspended') {
@@ -19,7 +22,8 @@ function startAudioContext() {
     }
 }
 
-window.onload = function() {
+// Function to initialize the audio and UI
+function initAudio() {
     startAudioContext();
 
     oscillator = audioContext.createOscillator();
@@ -41,13 +45,14 @@ window.onload = function() {
     filterEnvelope.gain.value = 0; // Initialize envelope gain to 0
     filterEnvelope.connect(filter.frequency);
 
+    // Create step buttons
     const grid = document.getElementById('grid');
     for (let i = 0; i < 16; i++) {
         const button = document.createElement('button');
         button.textContent = 'Step ' + (i + 1);
         button.addEventListener('click', function() {
             const step = i;
-            const frequency = 440 * Math.pow(2, (step - 9) / 12); // Convert step to frequency
+            const frequency = 220; // Adjust the base frequency as needed
 
             if (sequence[step] === frequency) {
                 sequence[step] = null;
@@ -73,30 +78,7 @@ window.onload = function() {
         grid.appendChild(button);
     }
 
-    stepInterval = setInterval(() => {
-        const sequenceValue = sequence[stepIndex];
-        
-        if (playingSteps.length > 0) {
-            // Check if there are active steps
-            if (!currentOscillator) {
-                // Create a new oscillator if none exists
-                currentOscillator = audioContext.createOscillator();
-                currentOscillator.type = 'sawtooth';
-                currentOscillator.connect(filter);
-                currentOscillator.start(0);
-            }
-            
-            // Set the frequency
-            currentOscillator.frequency.value = sequenceValue !== undefined ? sequenceValue : 0;
-        } else if (currentOscillator) {
-            // Stop the current oscillator if no active steps
-            currentOscillator.stop(0);
-            currentOscillator = null;
-        }
-        
-        stepIndex = (stepIndex + 1) % stepSequencer.length;
-    }, 60000 / tempo);
-
+    // Create event listeners for audio parameter controls
     ['cutoff', 'resonance', 'envelope', 'decay', 'accent'].forEach(control => {
         document.getElementById(control).addEventListener('input', function() {
             if (this.id === 'cutoff') {
@@ -109,6 +91,7 @@ window.onload = function() {
         });
     });
 
+    // Create event listener for tempo control
     document.getElementById('tempo').addEventListener('input', function() {
         if (this.value !== undefined) {
             tempo = this.value;
@@ -120,57 +103,48 @@ window.onload = function() {
             }, 60000 / tempo);
         }
     });
- 
+
+    // Create event listener for tuning control
     document.getElementById('tuning').addEventListener('input', function() {
         const tuning = parseInt(this.value);
-    
+
         // Apply tuning to each step in the sequence
         for (let i = 0; i < 16; i++) {
             const baseFrequency = 440; // Adjust the base frequency as needed
             const scaleFactors = [1, 1.0595, 1.1225, 1.1892, 1.2599]; // Adjust scale factors for your scale
             let frequency = baseFrequency * scaleFactors[i % scaleFactors.length];
-    
+
             // Apply tuning offset
             frequency *= Math.pow(2, tuning / 12);
-    
+
             // Update the sequence and stepSequencer arrays
             sequence[i] = frequency;
             stepSequencer[i] = frequency;
         }
     });
 
-    // Initialize swing value
-
+    // Create event listener for swing control
     document.getElementById('swing').addEventListener('input', function() {
         swing = parseFloat(this.value);
     });
-    
 
-    document.getElementById('tempo').addEventListener('input', function() {
-        if (this.value !== undefined) {
-            tempo = this.value;
-            clearInterval(stepInterval);
-            stepInterval = setInterval(() => {
-                const sequenceValue = sequence[stepIndex];
-                oscillator.frequency.value = sequenceValue !== undefined ? sequenceValue : 0;
-    
-                // Add a slight offset to every other beat
-                const offset = stepIndex % 2 === 0 ? swing : -swing; // Adjust the offset using the swing variable
-                const currentTime = audioContext.currentTime + offset;
-                filterEnvelope.gain.cancelScheduledValues(currentTime);
-                filterEnvelope.gain.setValueAtTime(filterEnvelope.gain.value, currentTime);
-                filterEnvelope.gain.linearRampToValueAtTime(1, currentTime + 0.05); // Attack time (adjust as needed)
-    
-                stepIndex = (stepIndex + 1) % stepSequencer.length;
-            }, 60000 / tempo);
-        }
-    });
-
-    
+    // Create event listener for start/stop button
     const startStopButton = document.getElementById('start-stop');
-    startStopButton.addEventListener('click', function() {
+    let currentOscillators = Array(16).fill(null);
+
+    startStopButton.addEventListener('click', function () {
         if (startStopButton.textContent === 'Start') {
             startAudioContext();
+
+            // Ensure all oscillators are stopped and disconnected
+            currentOscillators.forEach((oscillator, index) => {
+                if (oscillator) {
+                    oscillator.stop(0);
+                    oscillator.disconnect();
+                    currentOscillators[index] = null;
+                }
+            });
+
             oscillator = audioContext.createOscillator();
             gainNode = audioContext.createGain();
             filter = audioContext.createBiquadFilter();
@@ -183,14 +157,44 @@ window.onload = function() {
             filter.connect(gainNode);
             gainNode.connect(audioContext.destination);
             oscillator.start(0);
+            stepIndex = 0; // Reset the stepIndex to zero
+
             stepInterval = setInterval(() => {
-                const sequenceValue = sequence[stepIndex];
-                oscillator.frequency.value = sequenceValue !== undefined ? sequenceValue : 0;
+                const sequenceValue = stepSequencer[stepIndex];
+
+                if (sequenceValue !== undefined) {
+                    if (currentOscillators[stepIndex] === null) {
+                        currentOscillators[stepIndex] = audioContext.createOscillator();
+                        currentOscillators[stepIndex].type = 'sawtooth';
+                        currentOscillators[stepIndex].connect(filter);
+                        currentOscillators[stepIndex].start(0);
+                    }
+                    currentOscillators[stepIndex].frequency.setValueAtTime(sequenceValue, audioContext.currentTime);
+                } else {
+                    // Stop and disconnect the oscillator for deactivated steps
+                    if (currentOscillators[stepIndex]) {
+                        currentOscillators[stepIndex].stop(0);
+                        currentOscillators[stepIndex].disconnect();
+                        currentOscillators[stepIndex] = null;
+                    }
+                }
+
                 stepIndex = (stepIndex + 1) % stepSequencer.length;
             }, 60000 / tempo);
+
             startStopButton.textContent = 'Stop';
         } else {
             clearInterval(stepInterval);
+
+            // Ensure all oscillators are stopped and disconnected
+            currentOscillators.forEach((oscillator, index) => {
+                if (oscillator) {
+                    oscillator.stop(0);
+                    oscillator.disconnect();
+                    currentOscillators[index] = null;
+                }
+            });
+
             oscillator.stop(0);
             startStopButton.textContent = 'Start';
         }
